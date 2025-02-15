1f26a501ddef47d6a71780a8feb97fe38ae659af
ravikth <ravi.kakadia@techholding.co>
2025-02-14 16:23:15 -0800

    Added Poppins font for Navbar Applied Global css Applied Custom Navbar css

--- frontend/web-app/src/Components/NavBar.tsx
+++ frontend/web-app/src/Components/NavBar.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom';
+import '../Styles/custom/navBar.css'
 
 function NavBar() {
-  return (
-    <div>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/faq">FAQ</Link>
-      </div>
-      <Outlet />
-    </div>
-  )
+	return (
+		<>
+			<div className='navbar'>
+				<div>Sproutly</div>
+				<div className='navbar-link-container'>
+					<Link className='navbar-link text-white' to="/">Home</Link>
+					<Link className='navbar-link' to="/about">About</Link>
+					<Link className='navbar-link' to="/faq">FAQ</Link>
+				</div>
+				<div className='navbar-login-logout-button'>Log In</div>
+			</div>
+			<Outlet />
+		</>
+	)
 };
 
 export default NavBar;
