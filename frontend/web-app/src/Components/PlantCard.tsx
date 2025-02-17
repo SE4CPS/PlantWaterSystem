a18d32335a65e4aa40748fb063e86a6323dc5c5a
ravi-p-k1 <ravipkakadia1@gmail.com>
2025-02-16 21:25:07 -0800

    Added Plant Card component skeleton design Added Home Page skeleton design

---
+++ frontend/web-app/src/Components/PlantCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/custom/plantCard.css'
+
+function PlantCard() {
+  return (
+    <div className='plantCard'>
+        
+    </div>
+  )
+}
+
+export default PlantCard
