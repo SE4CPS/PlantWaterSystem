75f531bbf3f1843297020ff030fd78ce02477f64
ravi-p-k1 <ravipkakadia1@gmail.com>
2025-02-18 12:00:33 -0800

    Added Sproutly Logo to Navbar

--- frontend/web-app/src/Components/NavBar.tsx
+++ frontend/web-app/src/Components/NavBar.tsx
@@ -1,6 +1,8 @@
 import React, { useMemo } from 'react'
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import '../Styles/custom/navBar.css'
+import sproutlyLogo from '../Images/sproutly-logo.svg';
+import sproutlyText from '../Images/sproutly-text.svg';
 
 function NavBar() {
 
@@ -26,7 +28,10 @@ function NavBar() {
 	return (
 		<>
 			<div className='navbar'>
-				<div>Sproutly</div>
+				<div>
+					<img className='sproutly-logo' src={sproutlyLogo} alt='error img'/>
+					<img className='sproutly-text' src={sproutlyText} alt='error img'/>
+				</div>
 				<div className='navbar-link-container'>
 					{pageArray.map((page, index)=>{
 						return <Link key={index} className={`navbar-link${location.pathname===page.path? ' text-white': ''}`} to={page.path}>{page.name}</Link>
