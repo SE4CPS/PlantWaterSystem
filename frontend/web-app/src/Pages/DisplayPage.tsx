08637b5a51b084690027da8327a65289af1e1540
ravi-p-k1 <ravipkakadia1@gmail.com>
2025-02-18 19:01:32 -0800

    Edited display page router as children of navbar Added display design with CSS Added font-poppins in global CSS

--- frontend/web-app/src/Pages/DisplayPage.tsx
+++ frontend/web-app/src/Pages/DisplayPage.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import '../Styles/custom/displayPage.css'
+import display_img_1 from '../Images/display-1.png'
+import display_img_2 from '../Images/display-2.png'
+import display_img_3 from '../Images/display-3.png'
 
 function DisplayPage() {
   return (
-    <div>DisplayPage</div>
+    <div className='displayPage font-poppins'>
+      <div className='displayPage-text-decoration'>
+        <div className='displayPage-sales-text'>
+          Tracking with Sproutly <b className='highlighted-text'>helps your plants thrive</b> by keeping them healthy, hydrated & happy.
+        </div>
+        <div className='displayPage-login-button'>
+          Log In
+        </div>
+        <div className='displayPage-signup-text'>
+          Don't have an account? <div className='signup-text'>Sign Up here</div>
+        </div>
+      </div>
+      <div className='displayPage-image-decoration'>
+        <div className='displayPage-upper-image'>
+          <img className='displayPage-image-3' src={display_img_3} alt='error img' />
+          <img className='displayPage-image-2' src={display_img_2} alt='error img' />
+        </div>
+        <img className='displayPage-image-1' src={display_img_1} alt='error img' />
+      </div>
+    </div>
   )
 }
 
