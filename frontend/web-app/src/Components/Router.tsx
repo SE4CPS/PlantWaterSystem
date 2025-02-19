0978bc731cddf917e0632b9cee7002750f1110ec
Nakul Bhandare <nakulbhandare@Nakuls-MacBook-Air.local>
2025-02-18 23:08:20 -0800

    feat: added docker compose for all three components

--- frontend/web-app/src/Components/Router.tsx
+++ frontend/web-app/src/Components/Router.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "../Pages/LoginPage";
 import HomePage from "../Pages/HomePage";
@@ -5,12 +6,9 @@ import PlantDetailPage from "../Pages/PlantDetailPage";
 import AboutPage from "../Pages/AboutPage";
 import FaqPage from "../Pages/FaqPage";
 import ErrorPage from "../Pages/ErrorPage";
-<<<<<<< HEAD
-=======
 import NavBar from "./NavBar";
 import SignUpPage from "../Pages/SignUpPage";
 import DisplayPage from "../Pages/DisplayPage";
->>>>>>> fe68087a11103b291e8a14f454bdff774de1db60
 
 const router = createBrowserRouter([
     {
@@ -21,7 +19,6 @@ const router = createBrowserRouter([
         element: <LoginPage/>,
     },
     {
-<<<<<<< HEAD
         path: '/',
         element: <HomePage/>,
     },
@@ -36,8 +33,8 @@ const router = createBrowserRouter([
     {
         path: '/faq',
         element: <FaqPage />,
-    }
-=======
+    },
+    {
         path: '/signup',
         element: <SignUpPage />,
     },
@@ -67,7 +64,6 @@ const router = createBrowserRouter([
             }
         ]
     },
->>>>>>> fe68087a11103b291e8a14f454bdff774de1db60
 ]);
 
 function Provider() {
