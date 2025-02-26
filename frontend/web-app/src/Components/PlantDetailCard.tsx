fa827974993ddd751c7d9e4b58f4f3daf90b0d57
ravi-p-k1 <ravipkakadia1@gmail.com>
2025-02-26 10:36:09 -0800

    PlantDetail page design done Added PlantDetailCard component Added plant card close btn icon Added plantDetailPage table CSS

--- frontend/web-app/src/Components/PlantDetailCard.tsx
+++ frontend/web-app/src/Components/PlantDetailCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import closeBtn from '../Images/plant-card-close-btn-icon.svg'
 import dummyImage from '../Images/rose.png'
 
 function PlantDetailCard({status}: {status: string}) {
@@ -23,13 +24,65 @@ function PlantDetailCard({status}: {status: string}) {
                 </div>
             </div>
             <div className='plant-detail-card-button-container'>
-                <img className='plant-detail-card-close-button' src='' alt='error img'/>
+                <img className='plant-detail-card-close-button' src={closeBtn} alt='error img'/>
                 <button className='plant-detail-card-edit-button'>Edit</button>
                 <button className='plant-detail-card-delete-button'>Delete</button>
             </div>
         </div>
         <div className='plant-detail-card-history'>
-
+            <div className='title'>
+                Water History
+            </div>
+            <div className='history-table-container'>
+                <div className='history-table'>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>ADC Value</th>
+                            <th>Moisture Level</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>2/2/2025</td>
+                            <td>2:23 pm</td>
+                            <td>43243</td>
+                            <td>100%</td>
+                            <td>Good</td>
+                        </tr>
+                        <tr>
+                            <td>2/2/2025</td>
+                            <td>2:23 pm</td>
+                            <td>43243</td>
+                            <td>100%</td>
+                            <td>Good</td>
+                        </tr>
+                        <tr>
+                            <td>2/2/2025</td>
+                            <td>2:23 pm</td>
+                            <td>43243</td>
+                            <td>100%</td>
+                            <td>Good</td>
+                        </tr>
+                        <tr>
+                            <td>2/2/2025</td>
+                            <td>2:23 pm</td>
+                            <td>43243</td>
+                            <td>100%</td>
+                            <td>Good</td>
+                        </tr>
+                        <tr>
+                            <td>2/2/2025</td>
+                            <td>2:23 pm</td>
+                            <td>43243</td>
+                            <td>100%</td>
+                            <td>Good</td>
+                        </tr>
+                    </tbody>
+                </div>
+            </div>
         </div>
     </div>
   )
