96d53f576e501149209e2c69882abb3c6e6847e5
Nakul Bhandare <nakulbhandare@Nakuls-MacBook-Air.local>
2025-03-03 17:39:17 -0800

    fix: read backend url from environment variable

--- frontend/web-app/src/Utils/axiosInstance.ts
+++ frontend/web-app/src/Utils/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // Dummy URL is used here we need to change it later.
 
 const axiosInstance = axios.create({
-    baseURL: 'http://sproutly.com/api',
+    baseURL: process.env.BACKEND_API_URL+'/api',
     timeout: 5000,
     headers: {
         "Content-Type": 'application/json',
