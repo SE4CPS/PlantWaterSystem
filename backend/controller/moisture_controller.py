2be353c078d3006b1f85cd8da349cce059221a06
Tin Phan <t_phan22@u.pacific.edu>
2025-03-24 16:21:17 -0700

    Match embedded-backend URL

--- backend/controller/moisture_controller.py
+++ backend/controller/moisture_controller.py
@@ -4,14 +4,12 @@ from services.sensor_service import get_service, SensorService
 from fastapi import  Depends
 from fastapi.responses import JSONResponse
 from typing import List
-# from fastapi.security import HTTPBasic, HTTPBasicCredentials
-# from services.user_service import get_user_service, UserService
 
 
 add_moisture_data = APIRouter()
 
 
-@add_moisture_data.post("/api/send-data", response_model=dict)
+@add_moisture_data.post("/api/sensor/data", response_model=dict)
 def add_moisture_entry(sensors: MoistureDataListSchema, service: SensorService = Depends(get_service)):
     try:
         # Call the service layer to add sensor moisture data
@@ -30,29 +28,7 @@ def add_moisture_entry(sensors: MoistureDataListSchema, service: SensorService =
         # If an unexpected error occurs during processing, return a 500 status code
         return JSONResponse(status_code=500, content={"status": "error", "message": f"Unexpected error: {str(e)}"})
 
-@add_moisture_data.post("/api/send-current", response_model=dict)
-async def send_current_data(request: Request, service: SensorService = Depends(get_service)):
-    try:
-        # Parse the incoming JSON data
-        data = await request.json()
-        sensors = data.get("sensor_data", [])
-
-        # Call the service layer to add sensor moisture data
-        response = service.receive_moisture_data(sensors)
-
-        # Check if the response contains an error
-        if "error" in response:
-            status_code = 400 if "Invalid Request" in response["error"] else 500
-            return JSONResponse(status_code=status_code, content={"status": "error", "error": response["error"]})
-
-        # If the response is successful, return the response with a 200 status code
-        return JSONResponse(status_code=200, content=response)
-
-    except Exception as e:
-        # If an unexpected error occurs during processing, return a 500 status code
-        return JSONResponse(status_code=500, content={"status": "error", "message": f"Unexpected error: {str(e)}"})
-
-@add_moisture_data.get("/api/send-current", response_model=dict)
+@add_moisture_data.get("/api/sensor/data", response_model=dict)
 async def get_current_data(service: SensorService = Depends(get_service)):
     try:
         # This is a placeholder for any logic to implement for GET requests.
