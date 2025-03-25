ef23f0590b40da4e2a89e639d42a77a5084ef089
Henglay-Eung <eunghenglay@gmail.com>
2025-03-25 10:29:44 -0700

    Read secret key from os

--- backend/config/authentication.py
+++ backend/config/authentication.py
@@ -5,8 +5,10 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
+import os
+
 # Secret key for signing JWTs
-SECRET_KEY = "sproutly"
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30  # Token expiry
 
