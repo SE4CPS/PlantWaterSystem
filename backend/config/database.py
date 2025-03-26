f8f131413bb34872e73e2cc017817da130b7e52c
Nakul Bhandare <57669292+nakulbhandare@users.noreply.github.com>
2025-03-26 11:18:27 -0700

    fix: add changes to increase connection pools for multiple request (#261)
    Co-authored-by: Nakul Bhandare <nakulbhandare@Nakuls-MacBook-Air.local>

--- backend/config/database.py
+++ backend/config/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = "postgresql://neondb_owner:npg_SbW8PXa4dpzK@ep-summer-poetry-a84hp8dp-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
-connection_pool = pool.SimpleConnectionPool(1, 10, DATABASE_URL)
+connection_pool = pool.SimpleConnectionPool(10, 200, DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 
