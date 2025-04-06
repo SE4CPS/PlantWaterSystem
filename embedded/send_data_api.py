ee0287e6a0b56c3bac4c850ec00b6b44ff07e99a
MayureshMore <m_more1@u.pacific.edu>
2025-04-05 19:54:17 -0700

    Updated API Config to use Curl

--- embedded/send_data_api.py
+++ embedded/send_data_api.py
@@ -136,7 +136,7 @@ def send_one_reading(url, reading):
             url,
             json=payload,
             headers={"Content-Type": "application/json"},
-            timeout=15,  # Using 15 seconds as in the old working code.
+            timeout=15,
         )
         if response.status_code == 200:
             logging.info(f"Reading {reading['id']} sent successfully.")
