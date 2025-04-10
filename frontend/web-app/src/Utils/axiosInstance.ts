dbf0bb7ebc7e02b34e323b51c49d119a5c0dda1a
ravi-p-k1 <ravipkakadia1@gmail.com>
2025-04-09 21:21:30 -0700

    - updated base url in axios instance - added error page and its CSS

--- frontend/web-app/src/Utils/axiosInstance.ts
+++ frontend/web-app/src/Utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://sproutly.com/api',
+    baseURL: 'http://dev.sprout-ly.com/api',
     timeout: 5000,
     headers: {
         "Content-Type": 'application/x-www-form-urlencoded',
