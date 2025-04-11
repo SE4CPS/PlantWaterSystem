3322aea837ac0f03649fd37287f040623e82c572
Nakul Bhandare <57669292+nakulbhandare@users.noreply.github.com>
2025-04-10 23:46:55 -0700

    Update axiosInstance.ts (#434)

--- frontend/web-app/src/Utils/axiosInstance.ts
+++ frontend/web-app/src/Utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://dev.sprout-ly.com/api',
+    baseURL: 'https://dev.sprout-ly.com/api',
     timeout: 5000,
     headers: {
         "Content-Type": 'application/x-www-form-urlencoded',
