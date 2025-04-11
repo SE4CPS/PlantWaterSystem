1e79ee87c6c3e7da0b383dd4368d937ec6bcf81f
ravi-p-k-1 <104999921+ravi-p-k-1@users.noreply.github.com>
2025-04-11 15:59:30 -0700

    Mvp2 changes (#444)
    - Replaced status with moisture level 
    - Added Github Link to About Page
    - Added FAQ page
    - Added Flexbox to Homepage
    - Added Loader

--- frontend/web-app/src/Components/PlantDetailCard.tsx
+++ frontend/web-app/src/Components/PlantDetailCard.tsx
@@ -7,19 +7,23 @@ import sensorController from '../Controller/SensorController'
 import handleApiError, { isAuthTokenInvalid } from '../Utils/apiService'
 import plantController from '../Controller/PlantController'
 import { toast } from 'react-toastify'
+import Loader from './Loader'
 
 function PlantDetailCard({plantMetaData}: {plantMetaData: PlantMetaData}) {
 
   const navigate = useNavigate();
 
   const [sensorTableData, setSensorTableData] = useState<Array<SensorTableData>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const deletePlant = async (sensorId: string) => {
     try {
+      setLoading(true);
       const response = await plantController.deletePlant(sensorId);
       toast.success(response.data.message, {
         position: 'top-right',
       })
+      setLoading(false);
       navigate('/app/dashboard');
     } catch (error: unknown) {
       if(isAuthTokenInvalid(error)) navigate('/');
@@ -30,8 +34,10 @@ function PlantDetailCard({plantMetaData}: {plantMetaData: PlantMetaData}) {
   useEffect(() => {
     const fetchSensorTableData = async () => {
       try {
+        setLoading(true);
         const response = await sensorController.getSensorData(plantMetaData.deviceId, plantMetaData.sensorId);
         setSensorTableData(response.data.data);
+        setLoading(false);
       } catch (error) {
         if(isAuthTokenInvalid(error)) navigate('/')
         handleApiError(error);
@@ -42,71 +48,72 @@ function PlantDetailCard({plantMetaData}: {plantMetaData: PlantMetaData}) {
   
 
   return (
-    <div className={`plant-detail-card font-poppins ${plantMetaData.status}`}>
-        <div className='plant-detail-card-information'>
-            <div className='detail-and-image-container'>
-                <img className='plant-detail-card-image' src={dummyImage} alt='error img'/>
-                <div className='plant-detail-card-details'>
-                    <div>
-                        Name: <b>{plantMetaData.name}</b>
-                    </div>
-                    <div>
-                        Status: <b>{plantMetaData.status}</b>
-                    </div>
-                    <div>
-                        Last Watered: <b>{sensorTableData.length!==0 ? sensorTableData[0].time: ''}</b> on <b>{sensorTableData.length!==0 ? sensorTableData[0].date: ''}</b>
-                    </div>
-                    <div>
-                        Sensor ID: <b>{plantMetaData.sensorId}</b>
-                    </div>
-                    <div>
-                        Device ID: <b>{plantMetaData.deviceId}</b>
-                    </div>
-                    {/* <div>
-                        Note: <b>Place this plant near the window</b>
-                    </div> */}
-                </div>
-            </div>
-            <div className='plant-detail-card-button-container'>
-                <img className='plant-detail-card-close-button' src={closeBtn} alt='error img' onClick={()=>navigate('/app/dashboard')}/>
-                {/* <button className='plant-detail-card-edit-button'>Edit</button> */}
-                <button onClick={()=>deletePlant(plantMetaData.sensorId)} className='plant-detail-card-delete-button'>Delete</button>
-            </div>
-        </div>
-        <div className='plant-detail-card-history'>
-            <div className='title'>
-                Water History
-            </div>
-            <div className='history-table-container'>
-                <div className='history-table'>
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Time</th>
-                            {/* <th>ADC Value</th>
-                            <th>Moisture Level</th> */}
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                          sensorTableData.map((data, index) => {
-                            return (
-                              <tr key={index}>
-                                <td>{data.date}</td>
-                                <td>{data.time}</td>
-                                {/* <td>{data.adcvalue}</td>
-                                <td>{data.moisture_level}</td> */}
-                                <td>{data.digitalsatus}</td>
-                              </tr>
-                            )
-                          })
-                        }
-                    </tbody>
-                </div>
-            </div>
-        </div>
-    </div>
+    <>
+      {loading && <Loader />}
+      <div className={`plant-detail-card font-poppins ${plantMetaData.moisture_level > 25 ? 'Wet':'Dry'}`}>
+          <div className='plant-detail-card-information'>
+              <div className='detail-and-image-container'>
+                  <img className='plant-detail-card-image' src={dummyImage} alt='error img'/>
+                  <div className='plant-detail-card-details'>
+                      <div>
+                          Name: <b>{plantMetaData.name}</b>
+                      </div>
+                      <div>
+                          Moisture Level: <b>{plantMetaData.moisture_level}%</b>
+                      </div>
+                      <div>
+                          Last Watered: <b>{sensorTableData.length!==0 ? sensorTableData[0].time: ''}</b> on <b>{sensorTableData.length!==0 ? sensorTableData[0].date: ''}</b>
+                      </div>
+                      <div>
+                          Sensor ID: <b>{plantMetaData.sensorId}</b>
+                      </div>
+                      <div>
+                          Device ID: <b>{plantMetaData.deviceId}</b>
+                      </div>
+                      {/* <div>
+                          Note: <b>Place this plant near the window</b>
+                      </div> */}
+                  </div>
+              </div>
+              <div className='plant-detail-card-button-container'>
+                  <img className='plant-detail-card-close-button' src={closeBtn} alt='error img' onClick={()=>navigate('/app/dashboard')}/>
+                  {/* <button className='plant-detail-card-edit-button'>Edit</button> */}
+                  <button onClick={()=>deletePlant(plantMetaData.sensorId)} className='plant-detail-card-delete-button'>Delete</button>
+              </div>
+          </div>
+          <div className='plant-detail-card-history'>
+              <div className='title'>
+                  Water History
+              </div>
+              <div className='history-table-container'>
+                  <div className='history-table'>
+                      <thead>
+                          <tr>
+                              <th>Date</th>
+                              <th>Time</th>
+                              {/* <th>ADC Value</th> */}
+                              <th>Moisture Level</th>
+                              {/* <th>Status</th> */}
+                          </tr>
+                      </thead>
+                      <tbody>
+                          {
+                            sensorTableData.map((data, index) => {
+                              return (
+                                <tr key={index}>
+                                  <td>{data.date}</td>
+                                  <td>{data.time}</td>
+                                  <td>{data.moisture_level}%</td>
+                                </tr>
+                              )
+                            })
+                          }
+                      </tbody>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </>
   )
 }
 
--- frontend/web-app/src/Pages/HomePage.tsx
+++ frontend/web-app/src/Pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import '../Styles/custom/homePage.css'
 import PlantCard from '../Components/PlantCard'
 import AddNewPlantCard from '../Components/AddNewPlantCard'
@@ -7,22 +7,31 @@ import { GetPlantData } from '../Interfaces/plantInterface'
 import handleApiError, { isAuthTokenInvalid } from '../Utils/apiService'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
+import Loader from '../Components/Loader'
 
 function HomePage() {
 
   const userstring = localStorage.getItem('userDetails');
   const user = userstring ? JSON.parse(userstring): {};
   const [plantData, setPlantData] = useState<Array<GetPlantData>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [loadedCount, setLoadedCount] = useState<number>(0);
   const navigate = useNavigate();
+
+  const isLoaded = useCallback(() => {
+    setLoadedCount(cnt => cnt+1);
+  }, [])
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
+        setLoading(true);
         const response = await plantController.getPlants();
         setPlantData(response.data);
         toast.success("User's plants fetched successfully", {
           position: 'top-right',
         })
+        setLoading(false);
       } catch (error:unknown) {
         if(isAuthTokenInvalid(error)) navigate('/');
         handleApiError(error)
@@ -31,13 +40,16 @@ function HomePage() {
     fetchUserData();
   }, [navigate])
 
+  const allLoaded = loadedCount === plantData.length;
+
   return (
     <div className='homePage'>
+      {(loading || !allLoaded) && <Loader />}
       <div className='nameBar'>{user.firstname || ''}&rsquo;s Plants</div>
       <div className='plantViewer'>
         {
           plantData.map((data, index)=>{
-            return <PlantCard key={index} name={data.plantname} sensorId={data.sensorid} deviceId={data.deviceid} />
+            return <PlantCard key={index} name={data.plantname} sensorId={data.sensorid} deviceId={data.deviceid} isLoaded={isLoaded} />
           })
         }
         <AddNewPlantCard/>
